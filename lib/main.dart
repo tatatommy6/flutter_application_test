79df832e8c603aee01cfd4bc7c5370867c020676
tatatommy6 <tatatommy6@naver.com>
2025-12-02 20:23:44 +0900

    main.dart 리팩토링
    위젯을 쪼개고
    메인 build 를 얕게 만들어 가독성을 높이는 작업 진행

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.blueAccent),
-      ),
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),useMaterial3: true),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -22,7 +20,9 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -30,7 +30,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _gotoNewScreen(){
+  void _gotoNewScreen() {
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => const NewScreen()),
@@ -43,8 +43,8 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  void _reset(){
-    setState((){
+  void _reset() {
+    setState(() {
       _counter = 0;
     });
   }
@@ -61,43 +61,61 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You have pushed the button this many times'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            const CounterDescription(),
+            CounterValue(value: _counter),
 
             const SizedBox(height: 20),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                FloatingActionButton(
-                  onPressed: _incrementCounter,
-                  heroTag: 'btn1',
-                  tooltip: 'increment',
-                  child: const Icon(Icons.add),
-                ),
 
-                const SizedBox(width:15),
-                FloatingActionButton(
-                  onPressed: _gotoNewScreen,
-                  heroTag: 'btn2',
-                  tooltip: 'go to a new screen',
-                  child: const Icon(Icons.navigate_next)
-                  ),
-
-                const SizedBox(width:15),
-                FloatingActionButton(
-                  onPressed: _reset,
-                  heroTag: 'btn3',
-                  tooltip: 'reset counter',
-                  child: const Icon(Icons.delete)
-                ),
-              ],
-            ),
+            CounterActions(onIncrement: _incrementCounter, onReset: _reset, onGoNext: _gotoNewScreen),
           ],
         ),
       ),
+    );  
+  }
+}
+class CounterDescription extends StatelessWidget{
+  const CounterDescription({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return const Text('you have pushed the button this many times', textAlign: TextAlign.center);
+  }
+}
+
+class CounterValue extends StatelessWidget{
+  const CounterValue({super.key, required this.value});
+
+  final int value;
+
+  @override
+  Widget build(BuildContext context){
+    return Text('$value', style: Theme.of(context).textTheme.headlineMedium);
+  }
+}
+
+class CounterActions extends StatelessWidget {
+  const CounterActions({
+    super.key,
+    required this.onIncrement,
+    required this.onReset,
+    required this.onGoNext
+    });
+
+  final VoidCallback onIncrement;
+  final VoidCallback onReset;
+  final VoidCallback onGoNext;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        FloatingActionButton(onPressed: onIncrement, heroTag: 'btn1', tooltip: 'increment', child: const Icon(Icons.add)),
+        const SizedBox(width: 15,),
+        FloatingActionButton(onPressed: onReset, heroTag: 'btn2', tooltip: 'reset numbers', child: const Icon(Icons.delete)),
+        const SizedBox(width: 15,),
+        FloatingActionButton(onPressed: onGoNext, heroTag: 'btn3', tooltip: 'go to next page', child: const Icon(Icons.navigate_next)),
+      ],
     );
   }
 }
