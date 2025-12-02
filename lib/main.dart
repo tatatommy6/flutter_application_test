1296e1f8c7fbcb1e174608ae7b3f56b225bdee3b
tatatommy6 <tatatommy6@naver.com>
2025-12-02 15:18:54 +0900

    들여쓰기 조정 및 가독성 높이기

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
 
   @override
   Widget build(BuildContext context) {
@@ -57,40 +56,43 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-    body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text('You have pushed the button this many times'),
-          Text(
-            '$_counter',
-            style: Theme.of(context).textTheme.headlineMedium,
-          ),
-          const SizedBox(height: 20),
 
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              FloatingActionButton(
-                onPressed: _incrementCounter,
-                heroTag: 'btn1',
-                tooltip: 'increment',
-                child: const Icon(Icons.add),
-              ),
-              const SizedBox(width:15),
-              FloatingActionButton(
-                onPressed: _gotoNewScreen,
-                heroTag: 'btn2',
-                tooltip: 'go to a new screen',
-                child: const Icon(Icons.navigate_next)
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('You have pushed the button this many times'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+
+            const SizedBox(height: 20),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                FloatingActionButton(
+                  onPressed: _incrementCounter,
+                  heroTag: 'btn1',
+                  tooltip: 'increment',
+                  child: const Icon(Icons.add),
                 ),
-              const SizedBox(width:15),
-              FloatingActionButton(
-                onPressed: _reset,
-                heroTag: 'btn3',
-                tooltip: 'reset counter',
-                child: const Icon(Icons.delete)
-              )
+
+                const SizedBox(width:15),
+                FloatingActionButton(
+                  onPressed: _gotoNewScreen,
+                  heroTag: 'btn2',
+                  tooltip: 'go to a new screen',
+                  child: const Icon(Icons.navigate_next)
+                  ),
+
+                const SizedBox(width:15),
+                FloatingActionButton(
+                  onPressed: _reset,
+                  heroTag: 'btn3',
+                  tooltip: 'reset counter',
+                  child: const Icon(Icons.delete)
+                ),
               ],
             ),
           ],
